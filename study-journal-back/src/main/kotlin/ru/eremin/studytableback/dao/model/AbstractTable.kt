4b1a838d1bb2b68ee5e446966e6a7c8a6477be52
Eremin Artem <1000001rtem@gmail.com>
2020-12-18 22:16:45 +0300

    feature: Добавлена аннотация для аудитора и документация

--- study-journal-back/src/main/kotlin/ru/eremin/studytableback/dao/model/AbstractTable.kt
+++ study-journal-back/src/main/kotlin/ru/eremin/studytableback/dao/model/AbstractTable.kt
@@ -4,11 +4,18 @@ import org.springframework.data.annotation.CreatedBy
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedBy
 import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.Instant
 import javax.persistence.Column
+import javax.persistence.EntityListeners
 import javax.persistence.MappedSuperclass
 
+/**
+ * Базовый набор полей для таблиц, автоматически заполняет
+ * необходимые служебные поля. Использует AuditingEntityListener для отслеживания изменений.
+ */
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
 abstract class AbstractTable {
 
     @CreatedBy
