832eeed016469654db7ecbd167a4865ff6a8a956
Eremin Artem <1000001rtem@gmail.com>
2020-12-18 16:44:03 +0300

    init: Созданы приложения
    - Spring-boot приложение для бэка
    - Angular приложение для фронта

---
+++ study-table-back/src/main/kotlin/ru/eremin/studytableback/StudyTableBackApplication.kt
@@ -0,0 +1,11 @@
+package ru.eremin.studytableback
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudyTableBackApplication
+
+fun main(args: Array<String>) {
+    runApplication<StudyTableBackApplication>(*args)
+}
