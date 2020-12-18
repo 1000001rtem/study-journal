9a2d8bc56680f26b1aba740356e92e7d29c47caa
Eremin Artem <1000001rtem@gmail.com>
2020-12-18 18:13:37 +0300

    feature: Доработан build.gradle.kts
    - Добавлены интеграционные тесты
    - Добавлены зависиомсти
    - Добавлен плагин для вычисления версии по гит тэгам

--- study-table-back/build.gradle.kts
+++ study-table-back/build.gradle.kts
@@ -1,15 +1,17 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    idea
     id("org.springframework.boot") version "2.4.1"
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
     kotlin("jvm") version "1.4.21"
     kotlin("plugin.spring") version "1.4.21"
     kotlin("plugin.jpa") version "1.4.21"
+    id("com.palantir.git-version") version "0.12.2"
 }
 
 group = "ru.eremin"
-version = "0.0.1"
+version = (extensions.extraProperties.get("gitVersion") as? groovy.lang.Closure<*>)?.call() ?: "dirty"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 repositories {
@@ -22,13 +24,42 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.9.9")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
     implementation("org.liquibase:liquibase-core")
-    runtimeOnly("com.h2database:h2")
     runtimeOnly("org.postgresql:postgresql")
+
+    testImplementation("com.h2database:h2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.3.2")
+    testImplementation("org.mockito:mockito-inline:2.8.47")
+    testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
+}
+
+sourceSets {
+    create("integrationTest") {
+        withConvention(org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet::class) {
+            kotlin.srcDir("src/integrationTest/kotlin")
+            resources.srcDir("src/integrationTest/resources")
+            compileClasspath += sourceSets["main"].output + configurations["testRuntimeClasspath"]
+            runtimeClasspath += output + compileClasspath + sourceSets["test"].runtimeClasspath
+            runtimeClasspath += output + compileClasspath + sourceSets["integrationTest"].runtimeClasspath
+        }
+    }
+}
+
+idea {
+    module {
+        testSourceDirs.addAll(kotlin.sourceSets["integrationTest"].resources.srcDirs)
+        testSourceDirs.addAll(kotlin.sourceSets["test"].resources.srcDirs)
+        testSourceDirs = testSourceDirs + sourceSets["integrationTest"].allJava.srcDirs
+        testResourceDirs = testResourceDirs + sourceSets["integrationTest"].resources.srcDirs
+    }
 }
 
 tasks.withType<KotlinCompile> {
@@ -40,4 +71,52 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+    testLogging.showStandardStreams = true
+    testLogging {
+        events("passed", "skipped", "failed")
+        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+        showExceptions = true
+        showCauses = true
+        showStackTraces = true
+    }
+    addTestListener(object : TestListener {
+        override fun beforeTest(p0: TestDescriptor?) = Unit
+        override fun beforeSuite(p0: TestDescriptor?) = Unit
+        override fun afterTest(desc: TestDescriptor, result: TestResult) = Unit
+        override fun afterSuite(desc: TestDescriptor, result: TestResult) {
+            printResults(desc, result)
+        }
+    })
+}
+
+task<Test>("integrationTest") {
+    description = "Runs the integration tests"
+    group = "verification"
+    testClassesDirs = sourceSets["integrationTest"].output.classesDirs
+    classpath = sourceSets["integrationTest"].runtimeClasspath
+    mustRunAfter(tasks["test"])
+    useJUnitPlatform()
+}
+
+tasks.named("check") {
+    dependsOn("integrationTest")
+}
+
+fun printResults(desc: TestDescriptor, result: TestResult) {
+    if (desc.parent != null) {
+        val output = result.run {
+            "Results: $resultType (" +
+                "$testCount tests, " +
+                "$successfulTestCount successes, " +
+                "$failedTestCount failures, " +
+                "$skippedTestCount skipped" +
+                ")"
+        }
+        val testResultLine = "|  $output  |"
+        val repeatLength = testResultLine.length
+        val separationLine = "-".repeat(repeatLength)
+        println(separationLine)
+        println(testResultLine)
+        println(separationLine)
+    }
 }
