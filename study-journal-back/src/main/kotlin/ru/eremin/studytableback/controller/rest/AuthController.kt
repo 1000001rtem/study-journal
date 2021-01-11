413e64c1dcbec60af49233e7a88335591bd2738d
Eremin Artem <1000001rtem@gmail.com>
2021-01-11 20:36:20 +0300

    feature: Исправлен ответ при авторизации

--- study-journal-back/src/main/kotlin/ru/eremin/studytableback/controller/rest/AuthController.kt
+++ study-journal-back/src/main/kotlin/ru/eremin/studytableback/controller/rest/AuthController.kt
@@ -4,7 +4,8 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import ru.eremin.studytableback.controller.dto.AuthRequest
-import ru.eremin.studytableback.controller.dto.AuthResponse
+import ru.eremin.studytableback.controller.dto.StudyJournalData
+import ru.eremin.studytableback.controller.dto.StudyJournalResponse
 import ru.eremin.studytableback.security.service.AuthenticationService
 
 @RestController
@@ -13,5 +14,8 @@ class AuthController(
 ) {
 
     @PostMapping(value = ["/auth"])
-    fun auth(@RequestBody authRequest: AuthRequest): AuthResponse = authenticationService.authenticate(authRequest)
+    fun auth(@RequestBody authRequest: AuthRequest): StudyJournalResponse =
+        StudyJournalData(
+            data = authenticationService.authenticate(authRequest)
+        )
 }
