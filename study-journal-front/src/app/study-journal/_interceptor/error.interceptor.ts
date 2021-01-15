19d96523663939253a1b28e3711fc8e734a15c51
Eremin Artem <1000001rtem@gmail.com>
2021-01-15 23:32:35 +0300

    feature/front: Добавлены интерсепторы

---
+++ study-journal-front/src/app/study-journal/_interceptor/error.interceptor.ts
@@ -0,0 +1,26 @@
+import {Injectable} from '@angular/core';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
+import {AuthenticationService} from '../_service/authentication.service';
+import {catchError} from 'rxjs/operators';
+
+/**
+ * Интерсептор перехватывающий ошибки
+ */
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+
+  constructor(private authenticationService: AuthenticationService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(catchError(err => {
+      if ([401, 403].indexOf(err.status) !== -1) {
+        this.authenticationService.logout();
+        // TODO: redirect to login page
+      }
+
+      const error = err.error.message || err.statusText;
+      return throwError(error);
+    }));
+  }
+}
--- study-journal-front/src/app/study-journal/study-journal.module.ts
+++ study-journal-front/src/app/study-journal/study-journal.module.ts
@@ -3,6 +3,9 @@ import {CommonModule} from '@angular/common';
 import {AuthorizedModule} from './authorized/authorized.module';
 import {UnauthorizedModule} from './unauthorized/unauthorized.module';
 import {StudyJournalComponent} from './study-journal.component';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {ErrorInterceptor} from './_interceptor/error.interceptor';
+import {TokenInterceptor} from './_interceptor/token.interceptor';
 
 
 @NgModule({
@@ -12,6 +15,11 @@ import {StudyJournalComponent} from './study-journal.component';
     AuthorizedModule,
     UnauthorizedModule
   ],
-  exports: [StudyJournalComponent]
+  exports: [StudyJournalComponent],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+  ]
 })
-export class StudyJournalModule { }
+export class StudyJournalModule {
+}
