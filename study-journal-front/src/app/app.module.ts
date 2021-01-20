3ad0ad39b58f178d5cf0e8aecc3626c4f5ab7654
Ерёмин Артём <av.eremin@i-teco.ru>
2021-01-20 20:31:48 +0300

    feature/front: Добавлены бибилиотеки bootstrap и material

--- study-journal-front/src/app/app.module.ts
+++ study-journal-front/src/app/app.module.ts
@@ -6,6 +6,8 @@ import {AppComponent} from './app.component';
 import {StudyJournalModule} from './study-journal/study-journal.module';
 import {ErrorComponent} from './error/error.component';
 import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import {HttpClientModule} from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     StudyJournalModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
