7ae4543e4df9a934d54c9ba510710266bae841a1
Eremin Artem <1000001rtem@gmail.com>
2021-01-26 20:50:38 +0300

    feature/front: Добавлена страница авторизации

--- study-journal-front/src/app/study-journal/unauthorized/login/login.component.ts
+++ study-journal-front/src/app/study-journal/unauthorized/login/login.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +8,26 @@ import {Component, OnInit} from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() {
+  MIN_PASSWORD_LENGTH = 6;
+
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
   }
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+        login: this.fb.control(null, [Validators.required, Validators.email]),
+        password: this.fb.control(null, [Validators.required, Validators.minLength(this.MIN_PASSWORD_LENGTH)])
+      }
+    );
   }
 
+  submit = () => {
+
+  };
+
+  logs = () => {
+    console.log(this.loginForm);
+  }
 }
--- study-journal-front/src/app/study-journal/unauthorized/unauthorized.module.ts
+++ study-journal-front/src/app/study-journal/unauthorized/unauthorized.module.ts
@@ -4,12 +4,24 @@ import {UnauthorizedComponent} from './unauthorized.component';
 import {LoginComponent} from './login/login.component';
 import {MainComponent} from './main/main.component';
 import {RegistrationComponent} from './registration/registration.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
 
 
 @NgModule({
   declarations: [UnauthorizedComponent, MainComponent, LoginComponent, RegistrationComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule
   ],
   exports: [UnauthorizedComponent]
 })
